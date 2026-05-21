a929f6978ab9cd0149491e81b3f5e6b864d09ef0
Israel Ramirez Martinez <israel977mz@gmail.com>
2026-05-21 12:59:07 -0600

    Primera parte

---
+++ MiTiendaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiTiendaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
